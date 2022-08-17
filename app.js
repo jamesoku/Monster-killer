571dcefa55720e8dc19c64661a03b0267f030397
James Oku <104051229+jamesoku@users.noreply.github.com>
2022-08-16 20:01:39 -0600

    added heal

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-import { attack1 } from './vendor.js';
-import { attack2 } from './vendor.js';
-import { W_attack } from './vendor.js';
-import { S_attack } from './vendor.js';
-import { P_Attack } from './vendor.js';
+import { MandP_attack,strongPA,PHeal } from './vendor.js';
+import { W_attack,S_attack } from './vendor.js';
+import { P_Attack, M_Attack, Heal } from './vendor.js';
+import { attack,P_Heal } from './vendor.js';
+
+Heal.addEventListener('click', function(){
+    let random = attack(PHeal.max,PHeal.Min)
+    P_Heal(random)
+    M_Attack()
+})
 
 W_attack.addEventListener('click', function(){
-    let random = attack1()
+    let random = attack(MandP_attack.max,MandP_attack.Min)
     P_Attack(random)
+    M_Attack()
 })
 
 
 S_attack.addEventListener('click', function(){
     
-    let random = attack2()
+    let random = attack(strongPA.max,strongPA.Min)
     P_Attack(random)
+    M_Attack()
 })
--- vendor.js
+++ vendor.js
@@ -1,27 +1,46 @@
 export let monbar = document.getElementById("Mon-bar")
+export let P_bar = document.getElementById("P_bar")
 export let W_attack = document.getElementById("attack") 
 export let S_attack = document.getElementById("S-attack") 
+export let Heal = document.getElementById("Heal")
 export const original_width = 40
-let next = original_width
+let next_p = original_width
+let next_M = original_width
+export const MandP_attack = { max: 23, Min: 7}
+export const strongPA = {max: 20, Min: 10}
+export const PHeal = {max: 27,Min: 10}
 
-export function attack1(){
-    let theRandomNumber = (((Math.floor(Math.random() * (15 - 5 + 1)) + 5)/100) * original_width);
+export function attack(max,min){
+    let theRandomNumber = (((Math.floor(Math.random() * (max - min + 1)) + min)/100) * original_width);
     return theRandomNumber
-
 }
-// attack 2 will be used by both player(Strong attack) and monster.
-export function attack2(){
-    let theRandomNumber = (((Math.floor(Math.random() * (25 - 5 + 1)) + 5)/100) * original_width);
-    return theRandomNumber
 
+export function M_Attack(){
+    let random = attack(MandP_attack.max,MandP_attack.Min)
+    next_M = next_M - random
+    console.log("-M",next_M)
+    if(next_M <= 0){
+        P_bar.style.width  = "0vw"
+    }
+
+    P_bar.style.width  = next_M+"vw"
 }
 
 export function P_Attack(random){
-    next = next - random
-    console.log(next)
-    if(next <= 0){
+    next_p = next_p - random
+    console.log("-P",next_p)
+    if(next_p <= 0){
         monbar.style.width  = "0vw"
     }
 
-    monbar.style.width  = next+"vw"
+    monbar.style.width  = next_p+"vw"
+}
+
+export function P_Heal(random){
+    next_M = next_M + random
+    console.log("-P",next_M)
+    if(next_M <= 0){
+        P_bar.style.width  = "0vw"
+    }
+    P_bar.style.width  = next_M+"vw"
 }
