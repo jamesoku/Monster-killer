fd89217947dbe445b91f4e6a47803d8c377dc9be
James Oku <104051229+jamesoku@users.noreply.github.com>
2022-08-17 19:13:48 -0600

    added heal

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 import { MandP_attack,strongPA,PHeal } from './vendor.js';
 import { W_attack,S_attack } from './vendor.js';
 import { P_Attack, M_Attack, Heal } from './vendor.js';
-import { attack,P_Heal } from './vendor.js';
+import { attack,P_Heal, checkwinner } from './vendor.js';
 
 Heal.addEventListener('click', function(){
     let random = attack(PHeal.max,PHeal.Min)
-    P_Heal(random)
-    M_Attack()
+    let M = P_Heal(random)
+    let p = M_Attack(random)
+    checkwinner(M,p)
 })
 
 W_attack.addEventListener('click', function(){
     let random = attack(MandP_attack.max,MandP_attack.Min)
-    P_Attack(random)
-    M_Attack()
+   let M = P_Attack(random)
+   let p = M_Attack()
+   checkwinner(M,p)
 })
 
 
 S_attack.addEventListener('click', function(){
     
     let random = attack(strongPA.max,strongPA.Min)
-    P_Attack(random)
-    M_Attack()
+    let M = P_Attack(random)
+   let p = M_Attack()
+   checkwinner(M,p)
 })
--- vendor.js
+++ vendor.js
@@ -14,33 +14,57 @@ export function attack(max,min){
     let theRandomNumber = (((Math.floor(Math.random() * (max - min + 1)) + min)/100) * original_width);
     return theRandomNumber
 }
+export function checkwinner(next_p,next_M){
+    console.log("-M",next_M)
+    console.log("-P",next_p)
+    if (next_p <= 0 && next_M > 0){
+        alert("Player won")
+    } 
+    else if (next_M <= 0 && next_p > 0){
+        alert("Monster won")
+    }
+    else if(next_M <= 0 && next_p <= 0) {
+        alert("Draw")
+
+    }
+
+
+}
 
 export function M_Attack(){
     let random = attack(MandP_attack.max,MandP_attack.Min)
     next_M = next_M - random
-    console.log("-M",next_M)
+    // console.log("-M",next_M)
     if(next_M <= 0){
+        next_M = 0
         P_bar.style.width  = "0vw"
     }
 
     P_bar.style.width  = next_M+"vw"
+    return next_M
 }
 
 export function P_Attack(random){
     next_p = next_p - random
-    console.log("-P",next_p)
+    // console.log("-P",next_p)
     if(next_p <= 0){
+        next_p = 0
         monbar.style.width  = "0vw"
     }
 
     monbar.style.width  = next_p+"vw"
+    return next_p
 }
 
 export function P_Heal(random){
     next_M = next_M + random
-    console.log("-P",next_M)
+    // console.log("-P",next_M)
+    if (next_M >= 40){
+        next_M = 40
+    }
     if(next_M <= 0){
         P_bar.style.width  = "0vw"
     }
     P_bar.style.width  = next_M+"vw"
+    return next_M
 }
